88cbc0b075463d1357adddba45f811b4d011e00a
TJ Uhler <terryuhler@gmail.com>
2021-05-12 16:49:31 -0500

    Added a platform to jump on

--- Pythought you were gone/Pygame/BasicPlatformer.py
+++ Pythought you were gone/Pygame/BasicPlatformer.py
@@ -17,14 +17,23 @@ Character.penup()
 Character.goto(-350, 0)
 Character.dy = -1.5
 
-#Platform
-Platform  = turtle.Turtle()
-Platform.speed(0)
-Platform.shape("square")
-Platform.color("#964B00")
-Platform.shapesize(stretch_wid = 19, stretch_len = 120)
-Platform.penup()
-Platform.goto(-590, -200)
+# Platform 1
+Platform1  = turtle.Turtle()
+Platform1.speed(0)
+Platform1.shape("square")
+Platform1.color("#964B00")
+Platform1.shapesize(stretch_wid = 19, stretch_len = 120)
+Platform1.penup()
+Platform1.goto(-590, -200)
+
+# Platform 2
+Platform2 = turtle.Turtle()
+Platform2.speed(0)
+Platform2.shape("square")
+Platform2.color("#964B00")
+Platform2.shapesize(3, 50)
+Platform2.penup()
+Platform2.goto(300, 20)
 
 # Character Moving Functions
 def move_left():
@@ -40,6 +49,12 @@ def move_right():
 def jump():
     y = Character.ycor()
     if y == 0:
+        y += 10
+        Character.sety(y)
+        y += 10
+        Character.sety(y)
+        y += 10
+        Character.sety(y)
         y += 10
         Character.sety(y)
         y += 10
@@ -68,10 +83,9 @@ while True:
         Character.sety(Character.ycor()+ Character.dy)
 
     #Platform Checking
-    if Character.ycor() <= Platform.ycor() + 199:
+    if Character.ycor() <= Platform1.ycor() + 199:
         Character.sety(0)
-
-
-
-
-
+    if Character.xcor() >= Platform2.xcor() - 512 and Character.ycor() <= Platform2.ycor():
+        Character.setx(-210)
+    if Character.ycor() <= Platform2.ycor() + 60 and Character.xcor() > -210:
+        Character.sety(60)
